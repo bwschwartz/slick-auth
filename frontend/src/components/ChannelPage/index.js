bd68d7187ada23de25209018c8a5f2e0523015cc
Benjamin Schwartz <bs4308@nyu.edu>
2022-10-14 08:58:27 -0400

    Revert "Fix channels page bug"
    This reverts commit 2b2dfd58a794fab4842ca04a38b0a8dc143c942f.

--- frontend/src/components/ChannelPage/index.js
+++ frontend/src/components/ChannelPage/index.js
@@ -1,28 +1,16 @@
-import { fetchChannels } from '../../store/channels'
+import { showChannels } from '../../store/channels'
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
-// import NavBar from '../NavBar'
+
+
 
 export const ChannelPage = () => {
 
   const dispatch = useDispatch();
-  const channelsList = useSelector(state => state.channels.channels)
-
-  console.log(channelsList)
-
-  // console.log(channelsList);
-
-  useEffect(() => {
-   dispatch(fetchChannels())
-  }, [])
-
-  const channelsArr = channelsList.map((channel, id) => (<li key={id}>{channel.title}</li>))
-
+  const channels = useSelector( (state) => state.channels )
   return (
     <>
-      <h1>Channels Page</h1>
-
-      <ul>{channelsArr}</ul>
+      <h1>Channels Page Bitch</h1>
+      {/* <ul>{channels}</ul> */}
     </>
   )
 }
