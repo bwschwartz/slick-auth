a60704a535561ffae1b04fa3ca490140ce63711f
Benjamin Schwartz <bs4308@nyu.edu>
2022-10-13 20:56:54 -0400

    Add channels page skeleton

--- frontend/src/components/ChannelPage/index.js
+++ frontend/src/components/ChannelPage/index.js
@@ -1,16 +1,23 @@
-import { showChannels } from '../../store/channels'
+import { fetchChannels } from '../../store/channels'
 import { useDispatch, useSelector } from 'react-redux'
-
-
+import { useEffect } from 'react'
 
 export const ChannelPage = () => {
 
   const dispatch = useDispatch();
-  const channels = useSelector( (state) => state.channels )
+  const channelsList = useSelector( state => Object.values(state.channels) )
+  // console.log(channelsList);
+
+  useEffect(() => {
+   dispatch(fetchChannels())
+  }, [])
+
+  const channelsArr = channelsList[0].map((channel, id) => (<li key={id}>{channel.title}</li>))
+
   return (
     <>
-      <h1>Channels Page Bitch</h1>
-      {/* <ul>{channels}</ul> */}
+      <h1>Channels Page</h1>
+      <ul>{channelsArr}</ul>
     </>
   )
 }
