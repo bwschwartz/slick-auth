5c2b84768f1517d07fdb579e76a76fcd1b0db795
Benjamin Schwartz <bs4308@nyu.edu>
2022-10-17 14:35:16 -0400

    Commit with working split screen

--- frontend/src/components/ChannelPage/index.js
+++ frontend/src/components/ChannelPage/index.js
@@ -1,24 +1,23 @@
-import { useEffect, useState, createRef } from 'react'
+import { useEffect, useCallback, useState, createRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Split from 'react-split'
 import { fetchChannels } from '../../store/channels'
 import './ChannelPage.css'
 
 const useChannelsWidth = (ref) => {
-  const [width, setWidth] = useState('1');
+  const [channelsWidth, setChannelsWidth] = useState(1);
   useEffect(() => {
     if (ref.current) {
-      const { current } = ref
-      const width = current.getBoundingClientRect().width
-      setWidth(Math.round(width))
+      const w = ref.current.getBoundingClientRect().width
+      setChannelsWidth(Math.round(w))
     }
   }, [ref])
-  return width
+  return channelsWidth
 }
 
 export const ChannelPage = () => {
   const channelsDivRef = createRef();
-  const width = useChannelsWidth(channelsDivRef)
+  const width = useChannelsWidth(channelsDivRef);
 
   const dispatch = useDispatch();
   const channelsObj = useSelector( (state) => state.channels ? Object.values(state.channels) : [] )
@@ -36,11 +35,16 @@ export const ChannelPage = () => {
       <div ref={channelsDivRef}>
         {width &&
         <>
-        {/* <h1 style={{color:"green"}}>{width}</h1> */}
+        <h1 style={{color:"green"}}>{width}</h1>
+        <div>
+           {/* <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" /> */}
+          <i type="caret-right"></i>
           <h5 id="channels-h5">Channels</h5>
-          <div id="channels-component">
+        </div>
+
+        <div id="channels-component">
             <ul id="channel-list">{channelsLis}</ul>
-          </div>
+        </div>
         </>}
       </div>
 
