4ea2116403abefd3a5954a99bc26e9443647cb68
Benjamin Schwartz <bs4308@nyu.edu>
2022-12-30 18:34:00 -0500

    Add filter functionality to dropdown list

--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -36,7 +36,21 @@ const NavBar = () => {
   }
 
   const filterList = () => {
-    console.log("i'm filtering honey")
+    let input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('search-input');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("dev-links");
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
   }
 
   return (
